df4e0b9a0af33b3998b8a9c015c96644147c5481
fabrizio411 <fabriziomourglia@gmail.com>
2023-06-11 11:47:56 -0300

    Create a function to check te result and add points

--- main.js
+++ main.js
@@ -10,6 +10,46 @@ const computer = {
     round_choice: ""
 }
 
+function checkResult(player_choice, computer_choice) {
+    if (player_choice === "rock") {
+        if (computer_choice === "rock") {
+            console.log("It's a draw");
+        } else if (computer_choice === "paper") {
+            console.log("You lost...  :(");
+            computer.points += 1;
+        } else if (computer_choice === "scissors") {
+            console.log("You WIN!!!  :D");
+            player.points += 1;
+        }
+
+    } else if (player_choice === "paper") {
+        if (computer_choice === "rock") {
+            console.log("You WIN!!!  :D");
+            player.points += 1;
+        } else if (computer_choice === "paper") {
+            console.log("It's a draw");
+        } else if (computer_choice === "scissors") {
+            console.log("You lost...  :(");
+            computer.points += 1;
+        }
+
+    } else if (player_choice === "scissors") {
+        if (computer_choice === "rock") {
+            console.log("You lost...  :(");
+            computer.points += 1;
+        } else if (computer_choice === "paper") {
+            console.log("You WIN!!!  :D");
+            player.points += 1;
+        } else if (computer_choice === "scissors") {
+            console.log("It's a draw");
+        }
+    } else {
+        console.log("Error")
+    }
+}
+
+
+
 
 while (player.points < 3 && computer.points < 3) {
     console.log("Score")
@@ -22,43 +62,7 @@ while (player.points < 3 && computer.points < 3) {
     let p_choice = player.round_choice;
     let c_choice = computer.round_choice;
 
-
-    if (p_choice === "rock") {
-        if (c_choice === "rock") {
-            console.log("It's a draw");
-        } else if (c_choice === "paper") {
-            console.log("You lost...  :(");
-            computer.points += 1;
-        } else if (c_choice === "scissors") {
-            console.log("You WIN!!!  :D");
-            player.points += 1;
-
-        }
-
-    } else if (p_choice === "paper") {
-        if (c_choice === "rock") {
-            console.log("You WIN!!!  :D");
-            player.points += 1;
-        } else if (c_choice === "paper") {
-            console.log("It's a draw");
-        } else if (c_choice === "scissors") {
-            console.log("You lost...  :(");
-            computer.points += 1;
-        }
-
-    } else if (p_choice === "scissors") {
-        if (c_choice === "rock") {
-            console.log("You lost...  :(");
-            computer.points += 1;
-        } else if (c_choice === "paper") {
-            console.log("You WIN!!!  :D");
-            player.points += 1;
-        } else if (c_choice === "scissors") {
-            console.log("It's a draw");
-        }
-    } else {
-        console.log("Error")
-    }
+    checkResult(p_choice, c_choice)
 
     if (computer.points === 3) {
         console.log("Final Score")
