62950be12b93db6c7a9b891eaeb5825873e86ab7
fabrizio411 <fabriziomourglia@gmail.com>
2023-06-13 18:14:06 -0300

    Create a test js file to adapt the code to display in web

---
+++ rock-paper-scissors/prueba.js
@@ -0,0 +1,45 @@
+const rockBTN = document.getElementById("rock");
+const paperBTN = document.getElementById("paper");
+const scissorsBTN = document.getElementById("scissors");
+const resultado = document.getElementById("resultado");
+
+
+const options = ["rock", "paper", "scissors"]
+let computer_choice = options[Math.floor(Math.random() * options.length)];
+let player_choice = "";
+
+
+rockBTN.addEventListener("click", () => {
+    player_choice = "rock";
+    if (computer_choice === "rock") {
+        resultado.innerHTML = "Draw";
+    } else if (computer_choice === "paper") {
+        resultado.innerHTML = "Loose";
+    }else if (computer_choice === "scissors") {
+        resultado.innerHTML = "Win";
+    }
+})
+
+paperBTN.addEventListener("click", () => {
+    player_choice = "paper";
+    if (computer_choice === "rock") {
+        resultado.innerHTML = "Win";
+    } else if (computer_choice === "paper") {
+        resultado.innerHTML = "Draw";
+    }else if (computer_choice === "scissors") {
+        resultado.innerHTML = "Loose";
+    }
+})
+
+scissorsBTN.addEventListener("click", () => {
+    player_choice = "scissors";
+    if (computer_choice === "rock") {
+        resultado.innerHTML = "Loose";
+    } else if (computer_choice === "paper") {
+        resultado.innerHTML = "Win";
+    }else if (computer_choice === "scissors") {
+        resultado.innerHTML = "Draw";
+    }
+})
+
+
