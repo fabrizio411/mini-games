71d8fe4f7c6acfda374d7085ec4b5db82a40e6b1
fabrizio411 <fabriziomourglia@gmail.com>
2023-06-13 20:10:11 -0300

    Add animation to restult text

--- rock-paper-scissors/prueba.js
+++ rock-paper-scissors/prueba.js
@@ -2,12 +2,16 @@ const rockBTN = document.getElementById("rock");
 const paperBTN = document.getElementById("paper");
 const scissorsBTN = document.getElementById("scissors");
 const resultado = document.getElementById("resultado");
-const compChoice = document.getElementById("computer-choice")
+const compChoice = document.getElementById("computer-choice");
+const score = document.getElementById("score");
 
 
 const options = ["rock", "paper", "scissors"];
 let computer_choice = "";
 let player_choice = "";
+
+let player_score = 0;
+
 
 
 rockBTN.addEventListener("click", () => {
@@ -18,9 +22,15 @@ rockBTN.addEventListener("click", () => {
         resultado.innerHTML = "You Tied";
     } else if (computer_choice === "paper") {
         resultado.innerHTML = "You Lost";
+        player_score--;
     }else if (computer_choice === "scissors") {
         resultado.innerHTML = "You Win!";
+        player_score++;
     }
+    resultado.classList.remove("animation");
+    void resultado.offsetWidth;
+    resultado.classList.add("animation");
+    score.innerHTML = `${player_score}`;
 })
 
 paperBTN.addEventListener("click", () => {
@@ -29,11 +39,17 @@ paperBTN.addEventListener("click", () => {
     player_choice = "paper";
     if (computer_choice === "rock") {
         resultado.innerHTML = "You Win!";
+        player_score--;
     } else if (computer_choice === "paper") {
         resultado.innerHTML = "You Tied";
     }else if (computer_choice === "scissors") {
         resultado.innerHTML = "You Lost";
+        player_score++;
     }
+    resultado.classList.remove("animation");
+    void resultado.offsetWidth;
+    resultado.classList.add("animation");
+    score.innerHTML = `${player_score}`;
 })
 
 scissorsBTN.addEventListener("click", () => {
@@ -42,11 +58,17 @@ scissorsBTN.addEventListener("click", () => {
     player_choice = "scissors";
     if (computer_choice === "rock") {
         resultado.innerHTML = "You Lost";
+        player_score--;
     } else if (computer_choice === "paper") {
         resultado.innerHTML = "You Win!";
+        player_score++;
     }else if (computer_choice === "scissors") {
         resultado.innerHTML = "You Tied";
     }
+    resultado.classList.remove("animation");
+    void resultado.offsetWidth;
+    resultado.classList.add("animation");
+    score.innerHTML = `${player_score}`;
 })
 
 
