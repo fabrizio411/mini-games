7721c1034c367e96d4418a1292b96d7364f0a415
fabrizio411 <fabriziomourglia@gmail.com>
2023-06-13 19:29:29 -0300

    Computer choice display functionality

--- rock-paper-scissors/prueba.js
+++ rock-paper-scissors/prueba.js
@@ -2,15 +2,17 @@ const rockBTN = document.getElementById("rock");
 const paperBTN = document.getElementById("paper");
 const scissorsBTN = document.getElementById("scissors");
 const resultado = document.getElementById("resultado");
+const compChoice = document.getElementById("computer-choice")
 
 
-const options = ["rock", "paper", "scissors"]
+const options = ["rock", "paper", "scissors"];
 let computer_choice = "";
 let player_choice = "";
 
 
 rockBTN.addEventListener("click", () => {
     computer_choice = options[Math.floor(Math.random() * options.length)];
+    compChoice.innerHTML = `${computer_choice}`;
     player_choice = "rock";
     if (computer_choice === "rock") {
         resultado.innerHTML = "You Tied";
@@ -23,6 +25,7 @@ rockBTN.addEventListener("click", () => {
 
 paperBTN.addEventListener("click", () => {
     computer_choice = options[Math.floor(Math.random() * options.length)];
+    compChoice.innerHTML = `${computer_choice}`;
     player_choice = "paper";
     if (computer_choice === "rock") {
         resultado.innerHTML = "You Win!";
@@ -35,6 +38,7 @@ paperBTN.addEventListener("click", () => {
 
 scissorsBTN.addEventListener("click", () => {
     computer_choice = options[Math.floor(Math.random() * options.length)];
+    compChoice.innerHTML = `${computer_choice}`;
     player_choice = "scissors";
     if (computer_choice === "rock") {
         resultado.innerHTML = "You Lost";
