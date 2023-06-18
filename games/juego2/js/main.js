7f74036f7770111badb65699a6b1c419fc31c87f
fabrizio411 <fabriziomourglia@gmail.com>
2023-06-18 13:23:33 -0300

    Game actualize

--- games/juego2/js/main.js
+++ games/juego2/js/main.js
@@ -17,22 +17,123 @@ const gameSection = document.getElementById("game-section");
 const player = {
     life: 200,
     bullets: 0,
-
+    damage: 50,
+    magSize: 3,
 }
 // IA:
 const ia = {
     life: 200,
     bullets: 0,
-
+    damage: 50,
+    magSize: 6,
 }
 
-let damage = 50;
-
+let difficulty = "MEDIUM"
+let turn = -1;
 
 const options = ["RELOAD", "SHOOT", "SHIELD"];
 let ia_choice = "";
-let turn = -1;
 
+
+function updateData(pChoice, iaChoice){
+    turn++;
+
+    if (!canShoot(player)) shoot.disabled = true;
+    else shoot.disabled = false
+
+    if (!canReload(player)) reload.disabled = true;
+    else reload.disabled = false
+    
+    computerChoice.innerHTML = choiceDisplay(iaChoice);
+    computerLife.innerHTML = ia.life;
+    computerBullets.innerHTML = ia.bullets + "/" + ia.magSize;
+    if (!canReload(ia)) computerBullets.innerHTML += '<span class="isFull"> (FULL)</span>'; 
+
+    playerChoice.innerHTML = choiceDisplay(pChoice);
+    playerLife.innerHTML = player.life;
+    playerBullets.innerHTML = player.bullets + "/" + player.magSize; 
+    if (!canReload(player)) playerBullets.innerHTML += '<span class="isFull"> (FULL)</span>';
+
+    if (player.life <= 0 && ia.life <= 0) {
+        //
+    }
+    else if (player.life <= 0) {
+        gameSection.innerHTML = "";
+        gameSection.innerHTML = '<h2 class="game-over lost">You Lost</h2>'
+    } 
+    else if (ia.life <= 0) {
+        gameSection.innerHTML = "";
+        gameSection.innerHTML = '<h2 class="game-over win">You Win</h2>'
+    }
+
+    ia_choice = iaChoiceByLevel(difficulty);
+}
+
+
+function iaChoiceByLevel(difficulty) {
+    // TESTING 
+    // Dificultad para testing
+    if (difficulty === 0 || difficulty === "TEST") { 
+        return "RELOAD";
+    }
+
+    // VERY EASY
+    // Si no tiene balas recarga siempre
+    // Es mas probable que use RELOAD o SHIELD 
+    else if (difficulty === 1 || difficulty === "VERY-EASY") { 
+        if (!canShoot(ia)) return "RELOAD";
+        let opt = options[Math.floor(Math.random() * options.length)];
+        let cont = 0;
+        while (cont < 3 && opt === "SHOOT" && ia.bullets < round((ia.life/player.damage)/2)) {
+            opt = options[Math.floor(Math.random() * options.length)];
+            cont++;
+        }
+        return opt;
+    }
+    // EASY
+    else if (difficulty === 2 || difficulty === "EASY") {
+        if (!canShoot(ia)) return "RELOAD";
+        return options[Math.floor(Math.random() * options.length)];
+    }
+    // MEDIUM
+    else if (difficulty === 3 || difficulty === "MEDIUM") {
+        return iaChoiceByLevel(Math.floor(Math.random() * 10)) //x10 es por la dificultad RANDOM que va de 6 en adelante... 
+    }
+    // HARD
+    else if (difficulty === 4 || difficulty === "HARD") {
+        if (turn === 0) return "RELOAD";
+        if (!canShoot(ia)) {
+            let opt = options[Math.floor(Math.random() * options.length)];
+            while (opt === "SHOOT") { opt = options[Math.floor(Math.random() * options.length)]; }
+            return opt;
+        }
+        if (!canReload(ia)) {
+            let opt = options[Math.floor(Math.random() * options.length)];
+            while (opt === "RELOAD") { opt = options[Math.floor(Math.random() * options.length)]; }
+            return opt;
+        }
+        return options[Math.floor(Math.random() * options.length)];
+    }
+    // IMPOSSIBLE
+    else if (difficulty == 5 || difficulty == "IMPOSSIBLE") {
+        if (turn == 0) return "RELOAD";
+        if (!canShoot(ia)) {
+            let opt = options[Math.floor(Math.random() * options.length)];
+            while (opt == "SHOOT") { opt = options[Math.floor(Math.random() * options.length)]; }
+            return opt;
+        }
+        if (!canReload(ia)) {
+            let opt = options[Math.floor(Math.random() * options.length)];
+            while (opt == "RELOAD") { opt = options[Math.floor(Math.random() * options.length)]; }
+            return opt;
+        }
+        return options[Math.floor(Math.random() * options.length)];
+    }
+    // RANDOM
+    else if (difficulty >= 6 || difficulty == "RANDOM") {
+        return options[Math.floor(Math.random() * options.length)];
+    }
+}
 
 function choiceDisplay(Choice) {
     if (turn === 0) return ""
@@ -47,57 +148,13 @@ function choiceDisplay(Choice) {
     }
 }
 
-
-function iaChoiceByLevel(difficulty) {
-    if (difficulty == 0 || difficulty == "EASY") {
-        if (!canShoot(ia)) return "RELOAD";
-        return options[Math.floor(Math.random() * options.length)];
-    }
-    else if (difficulty == 1 || difficulty == "MEDIUM") {
-        return options[Math.floor(Math.random() * options.length)];
-    }
-    else if (difficulty == 2 || difficulty == "HARD") {
-        if (turn == 0) return "RELOAD";
-        if (!canShoot(ia)) {
-            let opt = options[Math.floor(Math.random() * options.length)];
-            while (opt == "SHOOT") { opt = options[Math.floor(Math.random() * options.length)]; }
-            return opt;
-        }
-        return options[Math.floor(Math.random() * options.length)];
-    }
-}
-
-function updateData(pChoice, iaChoice){
-    turn++;
-
-    if (!canShoot(player)) shoot.disabled = true;
-    else shoot.disabled = false
-    
-    computerChoice.innerHTML = choiceDisplay(iaChoice);
-    computerLife.innerHTML = ia.life;
-    computerBullets.innerHTML = ia.bullets; 
-
-    playerChoice.innerHTML = choiceDisplay(pChoice);
-    playerLife.innerHTML = player.life;
-    playerBullets.innerHTML = player.bullets;
-
-    if (player.life <= 0 && ia.life <= 0) {
-        
-    }
-    else if (player.life <= 0) {
-        gameSection.innerHTML = "";
-        gameSection.innerHTML = '<h2 class="game-over lost">You Lost</h2>'
-    } 
-    else if (ia.life <= 0) {
-        gameSection.innerHTML = "";
-        gameSection.innerHTML = '<h2 class="game-over win">You Win</h2>'
-    }
-
-    ia_choice = iaChoiceByLevel("HARD")
-}
-
 function canShoot (whoShoots) {
     if (!whoShoots.bullets > 0) return false;
+    else return true;
+}
+
+function canReload (whoReloads) {
+    if (whoReloads.bullets >= whoReloads.magSize) return false;
     else return true;
 }
 
@@ -109,14 +166,14 @@ updateData("", "");
 
 // Get User button input
 reload.addEventListener("click", () => {
-    player.bullets += 1;
+    if (canReload(player)) player.bullets += 1;
     
     if (ia_choice === "RELOAD") { // reload - reload -> user:b+1, IA:b+1
-        ia.bullets += 1;
+        if (canReload(ia)) ia.bullets += 1;
     } else if (ia_choice === "SHOOT") { // reload - shoot  -> user:b+1&v-1, Ia:b-1
         if (canShoot(ia)) {
             ia.bullets -= 1;
-            player.life -= damage;
+            player.life -= ia.damage;
         } 
     } else if (ia_choice === "SHIELD") { // reload - shield -> user:b+1, IA:~
         // nothing
@@ -129,15 +186,15 @@ reload.addEventListener("click", () => {
 shoot.addEventListener("click", () => { 
     if (canShoot(player)) {
         player.bullets -= 1;
-        if (ia_choice !== "SHIELD") ia.life -= damage;
+        if (ia_choice !== "SHIELD") ia.life -= player.damage;
     }
     
     if (ia_choice === "RELOAD") { // shoot - reload  -> user:v-1, IA:v-1&b+1
-        ia.bullets += 1;
+        if (canReload(ia)) ia.bullets = 1;
     } else if (ia_choice === "SHOOT") { // shoot - shoot   -> user:b-1&v-1, IA:b-1&v-1
         if (canShoot(ia)) {
             ia.bullets -= 1;
-            player.life -= damage;
+            player.life -= ia.damage;
         }
     } else if (ia_choice === "SHIELD") { // shoot - shield  -> user:b-1, IA:~
         // nothing
@@ -149,7 +206,7 @@ shoot.addEventListener("click", () => {
 
 shield.addEventListener("click", () => {
     if (ia_choice === "RELOAD") { // shield - reload -> user:~, IA:b+1
-        ia.bullets += 1;
+        if (canReload(ia)) ia.bullets += 1;
     } else if (ia_choice === "SHOOT") { // shield - shoot  -> user:~, IA:v-1
         if (canShoot(ia)) ia.bullets -= 1;
     } else if (ia_choice === "SHIELD") { // shield - shield -> user:~, IA:~
